67cd44209350fc3b9e0d01c107f56c63bc0413b0
root <root@lxvmapp003.cern.ch>
2020-04-03 14:13:29 +0200

    Set basic functionality

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from subprocess import Popen, PIPE
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World!"
+    product = 'comsol'
+    command = f'sudo -u epuentes /eos/project-e/engtools/ITadmintools/licencequerytools/Flex/lmstat -a -c /afs/cern.ch/project/cad/doc/axcad/licstats/data/cvs/licenses_licmon/{product}/license.dat | cat -v'
+
+    stdout, stderr = Popen(command, shell=True, stdout=PIPE).communicate()
+    return stdout
 
 app.run(port=5000)
